ff66288b6afadfb503bf55039802059685819175
Lisa <elbondarenko04@gmail.com>
2025-04-23 15:03:16 +0300

    Find out the reason for the infinite loop

--- src/main/java/org/example/linkedlist/MergeKSortedLists.java
+++ src/main/java/org/example/linkedlist/MergeKSortedLists.java
@@ -1,38 +1,75 @@
 package org.example.linkedlist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MergeKSortedLists {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode current = new ListNode();
-        ListNode minEl = new ListNode();
+        Integer minEl = findMinInRow(lists);
+        ListNode head = new ListNode();
+        if(minEl == null) {
+            return null;
+        }
+        ListNode current = lists[minEl];
+        head = current;
+
         while (true) {
             minEl = findMinInRow(lists);
-            minEl = minEl.next;
-
-
+            if(minEl == null) {
+                current.next = null;
+                break;
+            }
+            current.next = lists[minEl].next;
+            current = current.next;
+            lists[minEl] = lists[minEl].next;
 
 
         }
+
+        return head;
+
+
     }
 
-    private ListNode findMinInRow(ListNode[] lists) {
+    private Integer findMinInRow(ListNode[] lists) {
         ListNode current1;
         ListNode dummyNode = new ListNode();
         ListNode temp = new ListNode(Integer.MAX_VALUE);
+        int minIndex = temp.val;
 
         for(int i =0; i<lists.length; i++) {
             current1 = lists[i];
             if(current1.val <= temp.val) {
                 temp = current1;
+                minIndex = i;
             }
+
         }
-        if(temp.val == Integer.MAX_VALUE) {
+        if (minIndex == Integer.MAX_VALUE)
             return null;
-        } else {
-            dummyNode = temp;
-            dummyNode = dummyNode.next;
 
+        return minIndex;
+    }
+
+    public void printLinkedList(ListNode head) {
+        List<Integer> values = new ArrayList<>();
+        ListNode current = head;
+
+        while (current != null) {
+            values.add(current.val);
+            current = current.next;
         }
 
-        return temp;
+        System.out.println(values);
+    }
+
+    public static void main(String[] args) {
+        MergeKSortedLists mergeKSortedLists = new MergeKSortedLists();
+        ListNode head1 = new ListNode(1, new ListNode(4, new ListNode(5)));
+        ListNode head2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode head3 = new ListNode(2, new ListNode(6));
+        ListNode[] lists = {head1, head2, head3};
+       mergeKSortedLists.printLinkedList(mergeKSortedLists.mergeKLists(lists));
+
     }
 }
